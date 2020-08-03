fc5f7a33d802970402137ebfb52c94b6583946f6
shanon <13fritsh@gmail.com>
2020-08-03 15:27:06 -0500

    set up the main.java file

---
+++ src/snackBarApp/Main.java
@@ -0,0 +1,14 @@
+package snackBarApp;
+
+public class Main
+{
+    private static void workWithData()
+    {
+        System.out.println("Snack Bar App");
+    }
+
+    public static void main(String[] args)
+    {
+        workWithData();
+    }
+}
