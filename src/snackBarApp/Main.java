65ef24c1ee7c5bc954ff25e8c6dca6a81500a024
shanon <13fritsh@gmail.com>
2020-08-03 18:09:38 -0500

    added the snacks in the main.java

--- src/snackBarApp/Main.java
+++ src/snackBarApp/Main.java
@@ -8,6 +8,13 @@ public class Main
         VendingMachine vm1 = new VendingMachine("Food");
         VendingMachine vm2 = new VendingMachine("Drink");
         VendingMachine vm3 = new VendingMachine("Office");
+
+        //String name, int quanity, double cost, int vendingMachineId
+        Snack s1 = new Snack("Chips", 36, 1.75, vm1.getId());
+        Snack s2 = new Snack("Chocolate Bar", 36, 1.00, vm1.getId());
+        Snack s3 = new Snack("Pretzel", 30, 2.00, vm1.getId());
+        Snack s4 = new Snack("Soda", 24, 2.50, vm2.getId());
+        Snack s5 = new Snack("Water", 20, 2.75, vm2.getId());
     }
 
     public static void main(String[] args)
