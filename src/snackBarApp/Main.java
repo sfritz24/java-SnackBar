cfb9ec0817e333cc8e8b42085997ca35d9c7ed72
shanon <13fritsh@gmail.com>
2020-08-03 18:02:27 -0500

    added the 3 vending machines

--- src/snackBarApp/Main.java
+++ src/snackBarApp/Main.java
@@ -5,6 +5,9 @@ public class Main
     private static void workWithData()
     {
         System.out.println("Snack Bar App");
+        VendingMachine vm1 = new VendingMachine("Food");
+        VendingMachine vm2 = new VendingMachine("Drink");
+        VendingMachine vm3 = new VendingMachine("Office");
     }
 
     public static void main(String[] args)
